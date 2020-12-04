92b4e221ba3634eb5cf5fb2a7fb077c23d87cf8d
singram <singram@who.com>
2020-12-04 15:55:35 -0500

    Day 1 solutions

--- 2020/day-01/day-01-part-1.rb
+++ 2020/day-01/day-01-part-1.rb
@@ -3,4 +3,8 @@
 file_path = File.expand_path("../day-01-input.txt", __FILE__)
 input     = File.read(file_path)
 
+#Use of set is tempting but ignores the case where 1010 + 1010 = 2020
+arr = input.split("\n").map(&:to_i)
 
+pair =  arr.combination(2).find{|p| p[0]+p[1]==2020}
+puts pair[0] * pair[1]
--- 2020/day-01/day-01-part-2.rb
+++ 2020/day-01/day-01-part-2.rb
@@ -3,4 +3,8 @@
 file_path = File.expand_path("../day-01-input.txt", __FILE__)
 input     = File.read(file_path)
 
+#Use of set is tempting but ignores the case where 1010 + 1010 = 2020
+arr = input.split("\n").map(&:to_i)
 
+tripple =  arr.combination(3).find{|p| p.sum == 2020}
+puts tripple[0] * tripple[1] * tripple[2]
