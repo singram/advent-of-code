14dc3640b9018eb170a8da9715a7978511bc2d4e
singram <singram@who.com>
2020-12-04 16:28:53 -0500

    Day 2 solutions

--- 2020/day-02/day-02-part-1.rb
+++ 2020/day-02/day-02-part-1.rb
@@ -3,4 +3,10 @@
 file_path = File.expand_path("../day-02-input.txt", __FILE__)
 input     = File.read(file_path)
 
+valid = input.split("\n").select do |i|
+  c_min, c_max, ch, pwd = i.match(/(\d+)-(\d+)\s(\w):\s(\w+)/i).captures
+  ch_count = pwd.count(ch)
+  ch_count >= c_min.to_i && ch_count <= c_max.to_i
+end
 
+puts valid.count
--- 2020/day-02/day-02-part-2.rb
+++ 2020/day-02/day-02-part-2.rb
@@ -3,4 +3,9 @@
 file_path = File.expand_path("../day-02-input.txt", __FILE__)
 input     = File.read(file_path)
 
+valid = input.split("\n").select do |i|
+  pos1, pos2, ch, pwd = i.match(/(\d+)-(\d+)\s(\w):\s(\w+)/i).captures
+  ( pwd[pos1.to_i-1] == ch ) ^ ( pwd[pos2.to_i-1] == ch )
+end
 
+puts valid.count
