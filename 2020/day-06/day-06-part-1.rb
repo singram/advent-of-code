0f55f578ab46a920fbf3074989fdc197f15ba464
singram <singram@who.com>
2020-12-06 18:16:05 -0500

    Day 6 solutions

--- 2020/day-06/day-06-part-1.rb
+++ 2020/day-06/day-06-part-1.rb
@@ -3,4 +3,6 @@
 file_path = File.expand_path("../day-06-input.txt", __FILE__)
 input     = File.read(file_path)
 
+groups =  input.split("\n\n")
 
+puts groups.map{|grp| grp.gsub("\n",'').split('').uniq.count}.sum
--- 2020/day-06/day-06-part-2.rb
+++ 2020/day-06/day-06-part-2.rb
@@ -3,4 +3,7 @@
 file_path = File.expand_path("../day-06-input.txt", __FILE__)
 input     = File.read(file_path)
 
+groups =  input.split("\n\n")
 
+# Split each group into people, split each persons responses into an array of responses.  Logically and the responses together and count the result for a group.  Sum all the group counts together.
+puts groups.map{|grp| grp.split("\n").map{|person| person.split('')}.inject(:&).count}.sum
