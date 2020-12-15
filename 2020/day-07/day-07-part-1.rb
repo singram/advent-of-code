748743028807840f054e9319902252d724ce7b5a
singram <singram@who.com>
2020-12-15 18:31:46 -0500

    Day 7 solutions

--- 2020/day-07/day-07-part-1.rb
+++ 2020/day-07/day-07-part-1.rb
@@ -3,4 +3,19 @@
 file_path = File.expand_path("../day-07-input.txt", __FILE__)
 input     = File.read(file_path)
 
+# e.g vibrant yellow bags contain 5 dark salmon bags, 5 dull green bags, 3 light silver bags.
 
+RULES = {}
+input.split("\n").map{|line| line.gsub("\.",'').gsub(/\sbags?/,'').split(' contain ')}.each do |bag, contains|
+  RULES[bag] = {}
+  contains.split(', ').map{|rule| rule.split(' ',2)}.each{|rule_count, contained_bag| RULES[bag][contained_bag]=rule_count.to_i}
+end
+
+def can_contain?(bag, tgt)
+  return true if RULES[bag][tgt] && RULES[bag][tgt]> 0
+  remaining = RULES[bag].select{|b,cnt| cnt > 0 }.keys
+  return false if remaining.empty?
+  remaining.any?{|next_bag| can_contain?(next_bag, tgt)}
+end
+
+p RULES.keys.select{|bag| can_contain?(bag, 'shiny gold')}.count
--- 2020/day-07/day-07-part-2.rb
+++ 2020/day-07/day-07-part-2.rb
@@ -3,4 +3,17 @@
 file_path = File.expand_path("../day-07-input.txt", __FILE__)
 input     = File.read(file_path)
 
+# e.g vibrant yellow bags contain 5 dark salmon bags, 5 dull green bags, 3 light silver bags.
 
+RULES = {}
+input.split("\n").map{|line| line.gsub("\.",'').gsub(/\sbags?/,'').split(' contain ')}.each do |bag, contains|
+  RULES[bag] = {}
+  contains.split(', ').map{|rule| rule.split(' ',2)}.each{|rule_count, contained_bag| RULES[bag][contained_bag]=rule_count.to_i}
+end
+
+def count_bags(bag)
+  return 1 + (RULES[bag] ? RULES[bag].map{|contained_bag, cnt| count_bags(contained_bag) * cnt}.sum : 0)
+end
+
+# Note the minux 1 to remove the original bag from the count.
+pp count_bags('shiny gold') - 1
