b2d9e4e477e2478ded08b1b89995f35ba72709cd
singram <singram@who.com>
2020-12-17 08:52:38 -0500

    Day 8 solutions

--- 2020/day-08/day-08-part-1.rb
+++ 2020/day-08/day-08-part-1.rb
@@ -3,4 +3,17 @@
 file_path = File.expand_path("../day-08-input.txt", __FILE__)
 input     = File.read(file_path)
 
+inst = input.split("\n").map{|line| line.split(' ')}.map{|op, n| [op, n.to_i]}
 
+i = 0
+acc = 0
+
+while !inst[i].nil?
+  op, n = inst[i]
+  inst[i] = nil
+  acc += n if op == 'acc'
+  n = 1 if op == 'nop' || op == 'acc'
+  i += n 
+end
+
+pp acc
--- 2020/day-08/day-08-part-2.rb
+++ 2020/day-08/day-08-part-2.rb
@@ -3,4 +3,38 @@
 file_path = File.expand_path("../day-08-input.txt", __FILE__)
 input     = File.read(file_path)
 
+INST = input.split("\n").map{|line| line.split(' ')}.map{|op, n| [op, n.to_i]}.freeze
 
+
+def run_instructions(inst)
+  i = 0
+  acc = 0
+  success = false
+  while !inst[i].nil?
+    op, n = inst[i]
+    inst[i] = nil
+    acc += n if op == 'acc'
+    n = 1 if op == 'nop' || op == 'acc'
+    i += n
+  end
+  return acc, i==inst.size
+end
+
+(0...INST.size).each do |i|
+  # Deep copy original INST set as processing is destructive
+  # Could be re-written to be far cheaper by tracking instruction flips and modifying the run func to be non-mutative.
+  set = Marshal.load(Marshal.dump(INST))#INST.slice()
+  original = set[i][0]
+  if set[i][0] == 'nop'
+    set[i][0] = 'jmp'
+  elsif set[i][0] == 'jmp'
+    set[i][0] = 'nop'
+  else
+    next
+  end
+  acc, finished = run_instructions(set)
+  if finished
+    p acc
+    break
+  end
+end
