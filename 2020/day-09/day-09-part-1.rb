84d778a8ea294ab469ebfb101466af515f8de382
singram <singram@who.com>
2020-12-18 15:24:04 -0500

    Day 9 solutions

--- 2020/day-09/day-09-part-1.rb
+++ 2020/day-09/day-09-part-1.rb
@@ -3,4 +3,10 @@
 file_path = File.expand_path("../day-09-input.txt", __FILE__)
 input     = File.read(file_path)
 
+code_seq = input.split("\n").map(&:to_i)
 
+PRE_AMBLE=25
+code_seq.each_cons(PRE_AMBLE+1).select do |codes|
+  code = codes.pop
+  p code unless codes.combination(2).map(&:sum).any?{|s| s==code}
+end
--- 2020/day-09/day-09-part-2.rb
+++ 2020/day-09/day-09-part-2.rb
@@ -3,4 +3,19 @@
 file_path = File.expand_path("../day-09-input.txt", __FILE__)
 input     = File.read(file_path)
 
+code_seq = input.split("\n").map(&:to_i)
 
+TARGET = 88311122
+
+(0...code_seq.size).each do |start_i|
+  acc = 0
+  end_i = start_i
+  while acc < TARGET
+    acc += code_seq[end_i]
+    end_i += 1
+  end
+  if acc == TARGET
+    # Note that 2 answers are generated, the first is the one expected by AoC
+    p code_seq[start_i..end_i].min + code_seq[start_i..end_i].max
+  end
+end
