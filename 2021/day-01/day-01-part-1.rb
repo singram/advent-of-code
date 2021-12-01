3e119a6fc740ffb0c63d20f82c3fbe151e3fe5e6
Stuart Ingram <ingrams2@upmc.com>
2021-12-01 08:31:02 -0500

    Day 1 solutions

--- 2021/day-01/day-01-part-1.rb
+++ 2021/day-01/day-01-part-1.rb
@@ -3,4 +3,10 @@
 file_path = File.expand_path("../day-01-input.txt", __FILE__)
 input     = File.read(file_path)
 
+input = input.split.map(&:to_i)
 
+inc_count = 0
+input.each_cons(2) do |depth_n, depth_n1|
+    inc_count += 1 if depth_n1 > depth_n
+end
+pp inc_count
--- 2021/day-01/day-01-part-2.rb
+++ 2021/day-01/day-01-part-2.rb
@@ -3,4 +3,8 @@
 file_path = File.expand_path("../day-01-input.txt", __FILE__)
 input     = File.read(file_path)
 
+input = input.split.map(&:to_i)
 
+input = input.each_cons(3).map(&:sum)
+
+pp input.each_cons(2).map{ |depth_n, depth_n1| depth_n1 > depth_n ? 1 : 0 }.sum
