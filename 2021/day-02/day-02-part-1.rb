9755a12ccd85f752e594cfb1a46bfd8bc1363c5d
Stuart Ingram <ingrams2@upmc.com>
2021-12-02 08:20:24 -0500

    Day 2 solutions

--- 2021/day-02/day-02-part-1.rb
+++ 2021/day-02/day-02-part-1.rb
@@ -3,4 +3,20 @@
 file_path = File.expand_path("../day-02-input.txt", __FILE__)
 input     = File.read(file_path)
 
+input = input.split("\n").map{ | inst | inst.split(" ") }
 
+depth = 0
+horizontal = 0
+input.each do | dir, i |
+    i = i.to_i
+    case (dir)
+    when "forward"
+        horizontal += i
+    when "down"
+        depth += i
+    when "up"
+        depth -= i
+    end
+end
+
+pp depth * horizontal
--- 2021/day-02/day-02-part-2.rb
+++ 2021/day-02/day-02-part-2.rb
@@ -3,4 +3,22 @@
 file_path = File.expand_path("../day-02-input.txt", __FILE__)
 input     = File.read(file_path)
 
+input = input.split("\n").map{ | inst | inst.split(" ") }
 
+aim = 0
+depth = 0
+horizontal = 0
+input.each do | dir, i |
+    i = i.to_i
+    case (dir)
+    when "forward"
+        horizontal += i
+        depth += aim * i
+    when "down"
+        aim += i
+    when "up"
+        aim -= i
+    end
+end
+
+pp depth * horizontal
