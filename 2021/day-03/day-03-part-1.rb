4830d6383f96bcc929cc5295fd2394f0a6416132
Stuart Ingram <ingrams2@upmc.com>
2021-12-03 11:23:19 -0500

    Day 3 solutions

--- 2021/day-03/day-03-part-1.rb
+++ 2021/day-03/day-03-part-1.rb
@@ -3,4 +3,17 @@
 file_path = File.expand_path("../day-03-input.txt", __FILE__)
 input     = File.read(file_path)
 
+require 'matrix'
+input = input.split("\n").map{|s| s.split('').map(&:to_i)}
+
+diag_size = input.size
+counter = nil
+input.each do |bits|
+    counter ||= Vector[*Array.new(bits.size, 0)]
+    counter += Vector[*bits]
+end
+
+gamma = counter.to_a.map{|bit| bit > diag_size/2 ? '1' : '0'}.join.to_i(2)
+epsilon = counter.to_a.map{|bit| bit < diag_size/2 ? '1' : '0'}.join.to_i(2)
+pp gamma * epsilon
 
--- 2021/day-03/day-03-part-2.rb
+++ 2021/day-03/day-03-part-2.rb
@@ -3,4 +3,29 @@
 file_path = File.expand_path("../day-03-input.txt", __FILE__)
 input     = File.read(file_path)
 
+require 'matrix'
+input = input.split("\n").map{|s| s.split('').map(&:to_i)}
+
+def filter_numbers(list, most_common, bit_pos = 0)
+    bit_count = list.map{ |bits| bits[bit_pos] }.sum
+    mid_point = list.size/2.0
+    # Find common bit value
+    common_bit = (bit_count > mid_point) ? 1 : 0
+    # Flip if the least common is needed
+    common_bit = common_bit == 1 ? 0 : 1 unless most_common
+    # Resolve if equal
+    common_bit = most_common ? 1 : 0 if bit_count == mid_point
+    # Filter list by bit
+    new_list = list.select{ |bits| bits[bit_pos] == common_bit }
+    # Reduce until there is only one
+    new_list.size == 1 ? new_list[0] : filter_numbers(new_list, most_common, bit_pos+=1)
+end
+
+def bin_arr_to_i(a)
+    a.map(&:to_s).join.to_i(2)
+end
+
+oxygen_generator_rating = bin_arr_to_i(filter_numbers(input.dup, true))
+co2_scrubber_rating     = bin_arr_to_i(filter_numbers(input.dup, false))
+pp oxygen_generator_rating * co2_scrubber_rating
 
