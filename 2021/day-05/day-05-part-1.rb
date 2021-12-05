e113447dc25dbf4e6653e262ccf9f11c42fa50e1
Stuart Ingram <ingrams2@upmc.com>
2021-12-05 09:32:26 -0500

    Day 5 solutions

--- 2021/day-05/day-05-part-1.rb
+++ 2021/day-05/day-05-part-1.rb
@@ -3,4 +3,26 @@
 file_path = File.expand_path("../day-05-input.txt", __FILE__)
 input     = File.read(file_path)
 
+require 'matrix'
+vectors = input.split("\n").map{ |l| l.match(/(\d+),(\d+) -> (\d+),(\d+)/).captures.map(&:to_i)}
 
+# Filter to only vertical and horizontal vectors
+hv_vectors = vectors.select{ |v| v[0]==v[2] || v[1]==v[3] }
+
+coord_map = {}
+
+hv_vectors.each do |coordinates|
+    start_v = Vector[*coordinates.shift(2)]
+    end_v   = Vector[*coordinates.shift(2)]
+    step_v  = (end_v - start_v).normalize.round
+
+    while start_v != end_v do
+        coord_map[start_v] ||= 0
+        coord_map[start_v] += 1
+        start_v += step_v
+    end
+    coord_map[start_v] ||= 0
+    coord_map[start_v] += 1
+end
+
+pp coord_map.select{ |k,v| v > 1}.count
