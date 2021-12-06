c10d669423a947e528be62af0422a89f66529544
Stuart Ingram <ingrams2@upmc.com>
2021-12-06 07:30:23 -0500

    Day 6 solutions

--- 2021/day-06/day-06-part-2.rb
+++ 2021/day-06/day-06-part-2.rb
@@ -3,4 +3,16 @@
 file_path = File.expand_path("../day-06-input.txt", __FILE__)
 input     = File.read(file_path)
 
+fish_tracker = Array.new(9,0)
 
+input.split(',').map(&:to_i).each{ |i| fish_tracker[i] += 1 }
+
+days = 256
+
+days.times do
+    spawning_fish = fish_tracker.shift
+    fish_tracker[6] += spawning_fish
+    fish_tracker[8]  = spawning_fish
+end
+
+pp fish_tracker.sum
