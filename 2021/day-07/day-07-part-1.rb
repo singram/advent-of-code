12f6712b19649257a7638a4772621db0e381c0f1
Stuart Ingram <ingrams2@upmc.com>
2021-12-07 15:31:37 -0500

    Day 7 solutions

--- 2021/day-07/day-07-part-1.rb
+++ 2021/day-07/day-07-part-1.rb
@@ -3,4 +3,25 @@
 file_path = File.expand_path("../day-07-input.txt", __FILE__)
 input     = File.read(file_path)
 
+crab_positions   = input.split(',').map(&:to_i)
 
+class Array
+    def median
+        sorted = self.sort # required
+        midpoint = self.length / 2 # integer division
+        if self.length.even?
+            # median is mean of two values around the midpoint
+            sorted[midpoint-1, 2].sum / 2.0
+        else
+            sorted[midpoint]
+        end
+    end
+end
+
+def calc_fuel_for_position(crab_positions, target_position)
+    crab_positions.map{|p| (p-target_position).abs}.sum
+end
+
+target_position = crab_positions.median.to_i
+
+pp calc_fuel_for_position(crab_positions, target_position)
--- 2021/day-07/day-07-part-2.rb
+++ 2021/day-07/day-07-part-2.rb
@@ -3,4 +3,31 @@
 file_path = File.expand_path("../day-07-input.txt", __FILE__)
 input     = File.read(file_path)
 
+crab_positions   = input.split(',').map(&:to_i).sort!
 
+# sum of the consecutive integers
+def sum_of_integers(distance)
+    distance = distance.abs
+    ((distance+1)*distance)/2
+end
+
+def calc_fuel_for_position(crab_positions, target_position)
+    crab_positions.map{|p| sum_of_integers(p-target_position)}.sum
+end
+
+max_pos = crab_positions.max
+min_pos = crab_positions.min
+
+# Find lowest fuel position using binary search on the sorted crab positions
+while min_pos < max_pos
+    mid_pos = (min_pos + max_pos) / 2
+    max_pos_fuel = calc_fuel_for_position(crab_positions, max_pos)
+    min_pos_fuel = calc_fuel_for_position(crab_positions, min_pos)
+    if max_pos_fuel > min_pos_fuel
+        max_pos = mid_pos
+    else
+        min_pos = mid_pos
+    end
+end
+
+pp calc_fuel_for_position(crab_positions, min_pos)
