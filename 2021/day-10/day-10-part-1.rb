ffde312b24fc4c07d9327b90bc2eaf6cb7de3a8d
Stuart Ingram <ingrams2@upmc.com>
2021-12-10 12:01:20 -0500

    Day 10 solutions

--- 2021/day-10/day-10-part-1.rb
+++ 2021/day-10/day-10-part-1.rb
@@ -3,4 +3,29 @@
 file_path = File.expand_path("../day-10-input.txt", __FILE__)
 input     = File.read(file_path)
 
+lines = input.split("\n").map{|l| l.split('')}
 
+char_scores = {')' => 3,
+    ']' => 57,
+    '}' => 1197,
+    '>' => 25137}
+
+corrupt_chars = []
+lines.each do |line|
+    stack = []
+    line.each do |ch|
+        if ['(','[','{','<'].include?(ch)
+            stack.push ch
+        else
+            op_char = stack.pop
+            next if op_char == '(' && ch == ')'
+            next if op_char == '[' && ch == ']'
+            next if op_char == '{' && ch == '}'
+            next if op_char == '<' && ch == '>'
+            corrupt_chars << ch
+            break # line is corrupt
+        end
+    end
+end
+
+pp corrupt_chars.map{|c| char_scores[c]}.sum
--- 2021/day-10/day-10-part-2.rb
+++ 2021/day-10/day-10-part-2.rb
@@ -3,4 +3,33 @@
 file_path = File.expand_path("../day-10-input.txt", __FILE__)
 input     = File.read(file_path)
 
+lines = input.split("\n").map{|l| l.split('')}
 
+char_score = {'(' => 1,
+    '[' => 2,
+    '{' => 3,
+    '<' => 4}
+
+incomplete_rows = []
+lines.each do |line|
+    stack = []
+    line.each do |ch|
+        if ['(','[','{','<'].include?(ch)
+            stack.push ch
+        else
+            op_char = stack.pop
+            next if op_char == '(' && ch == ')'
+            next if op_char == '[' && ch == ']'
+            next if op_char == '{' && ch == '}'
+            next if op_char == '<' && ch == '>'
+            # line is corrupt
+            stack = nil
+            break 
+        end
+    end
+    incomplete_rows << stack if stack
+end
+
+scores = incomplete_rows.map{ |row| row.reverse.inject(0){ |score, c| (score*5)+char_score[c] } }.sort
+
+pp scores[scores.size/2]
