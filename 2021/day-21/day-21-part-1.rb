819bd57b7ca116103d415e12b887a92a9470d515
Stuart Ingram <ingrams2@upmc.com>
2021-12-26 09:34:32 -0500

    Day 21 Solution part 1

--- 2021/day-21/day-21-part-1.rb
+++ 2021/day-21/day-21-part-1.rb
@@ -3,4 +3,23 @@
 file_path = File.expand_path("../day-21-input.txt", __FILE__)
 input     = File.read(file_path)
 
+positions = input.split("\n").map{|line| line.split(' ')[-1].to_i - 1}
+scores = Array.new(positions.size){0}
 
+@d100 = 0
+
+def role_3_times
+    number = (3*@d100) + 1 + 2 + 3
+    @d100 += 3
+    number
+end
+
+(0...positions.size).cycle do |p|
+    moves = role_3_times
+    positions[p] += moves
+    positions[p] = positions[p] % 10 if positions[p] > 9 
+    scores[p] += positions[p] + 1
+    break if scores[p] >= 1000
+end
+
+pp scores.min * @d100
