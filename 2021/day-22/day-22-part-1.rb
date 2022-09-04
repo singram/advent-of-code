ec07db9b89f0008b9e032818f08924a1cf7106ec
Stuart Ingram <ingrams2@upmc.com>
2022-09-04 10:07:28 -0400

    Day 22 solutions

--- 2021/day-22/day-22-part-1.rb
+++ 2021/day-22/day-22-part-1.rb
@@ -1,57 +1,71 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
-file_path = File.expand_path("../day-22-input.txt", __FILE__)
+file_path = File.expand_path('day-22-input.txt', __dir__)
 input     = File.read(file_path)
 
 OFF = 0
 ON  = 1
-CUBE_RANGE = -50..50
+CUBE_RANGE = (-50..50).freeze
 
-insts = input.split("\n").map{|line| line.split(' ') }
-insts = insts.map{|line| [line[0] == 'on' ? ON : OFF, line[1].match(/x=(-?\d+)..(-?\d+),y=(-?\d+)..(-?\d+),z=(-?\d+)..(-?\d+)/).captures.map(&:to_i)]}
-
-class Range 
-    def overlaps?(other)
-        self.cover?(other.first) || other.cover?(self.first)
-    end
+insts = input.split("\n").map { |line| line.split(' ') }
+insts = insts.map do |line|
+  [line[0] == 'on' ? ON : OFF,
+   line[1].match(/x=(-?\d+)..(-?\d+),y=(-?\d+)..(-?\d+),z=(-?\d+)..(-?\d+)/).captures.map(&:to_i)]
 end
 
-class Cube 
-    attr_accessor :x_rng, :y_rng, :z_rng
-    def initialize(x1,x2,y1,y2,z1,z2)
-        self.x_rng = x1..x2
-        self.y_rng = y1..y2
-        self.z_rng = z1..z2
-    end
+# Monkey patching Range class
+class Range
+  # Returns true if the range supplied overlaps the current range.
+  def overlaps?(other)
+    cover?(other.first) || other.cover?(first)
+  end
+end
 
-    def overlaps?(cube)
-        self.x_rng.overlaps?(cube.x_rng) && self.y_rng.overlaps?(cube.y_rng) && self.z_rng.overlaps?(cube.z_rng)
-    end
+# Representation of a cube in 3d space
+class Cube
+  attr_accessor :x_rng, :y_rng, :z_rng
 
-    def trim_cube
-        Cube.new(*to_a.map {|r| r < CUBE_RANGE.first ? CUBE_RANGE.first : ( r > CUBE_RANGE.last ? CUBE_RANGE.last : r )})
-    end
-    
-    def write_to_map(cube_map, val)
-        x_rng.each do |x|
-            y_rng.each do |y|
-                z_rng.each do |z|
-                    cube_map[x+50][y+50][z+50] = val
-                end
-            end
+  def initialize(x1, x2, y1, y2, z1, z2)
+    self.x_rng = x1..x2
+    self.y_rng = y1..y2
+    self.z_rng = z1..z2
+  end
+
+  def overlaps?(cube)
+    x_rng.overlaps?(cube.x_rng) && y_rng.overlaps?(cube.y_rng) && z_rng.overlaps?(cube.z_rng)
+  end
+
+  def trim_cube
+    Cube.new(*to_a.map do |r|
+               if r < CUBE_RANGE.first
+                 CUBE_RANGE.first
+               else
+                 (r > CUBE_RANGE.last ? CUBE_RANGE.last : r)
+               end
+             end)
+  end
+
+  def write_to_map(cube_map, val)
+    x_rng.each do |x|
+      y_rng.each do |y|
+        z_rng.each do |z|
+          cube_map[x + 50][y + 50][z + 50] = val
         end
+      end
     end
+  end
 
-    def to_a
-        [x_rng.first, x_rng.last, y_rng.first, y_rng.last, z_rng.first, z_rng.last]
-    end
+  def to_a
+    [x_rng.first, x_rng.last, y_rng.first, y_rng.last, z_rng.first, z_rng.last]
+  end
 end
 
-insts = insts.map{|line| [line[0], Cube.new(*line[1]) ] }
-map = Array.new(CUBE_RANGE.size) { Array.new(CUBE_RANGE.size) { Array.new(CUBE_RANGE.size){ OFF } } }
+insts = insts.map { |line| [line[0], Cube.new(*line[1])] }
+map   = Array.new(CUBE_RANGE.size) { Array.new(CUBE_RANGE.size) { Array.new(CUBE_RANGE.size) { OFF } } }
 
-initialization_cube = Cube.new(-50,50,-50,50,-50,50)
-relevant_insts = insts.select{|i| i[1].overlaps?(initialization_cube)}
-relevant_insts.each{|i| i[1].write_to_map(map, i[0])}
+initialization_cube = Cube.new(-50, 50, -50, 50, -50, 50)
+relevant_insts      = insts.select { |i| i[1].overlaps?(initialization_cube) }
+relevant_insts.each { |i| i[1].write_to_map(map, i[0]) }
 
 pp map.flatten.count(ON)
--- 2021/day-22/day-22-part-2.rb
+++ 2021/day-22/day-22-part-2.rb
@@ -1,6 +1,124 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
-file_path = File.expand_path("../day-22-input.txt", __FILE__)
+file_path = File.expand_path('day-22-input.txt', __dir__)
 input     = File.read(file_path)
 
+OFF = 0
+ON  = 1
 
+instructions = input.split("\n").map { |line| line.split(' ') }
+instructions = instructions.map do |line|
+  [line[0] == 'on' ? ON : OFF,
+   line[1].match(/x=(-?\d+)..(-?\d+),y=(-?\d+)..(-?\d+),z=(-?\d+)..(-?\d+)/).captures.map(&:to_i)]
+end
+
+# Monkey patching Range class
+class Range
+  # Returns true if the range supplied overlaps the current range.
+  def overlaps?(other)
+    cover?(other.first) || other.cover?(first)
+  end
+end
+
+# Representation of a cube in 3d space
+class Cube
+  attr_accessor :x_rng, :y_rng, :z_rng
+
+  def initialize(x1_x2, y1_y2, z1_z2)
+    self.x_rng = x1_x2
+    self.y_rng = y1_y2
+    self.z_rng = z1_z2
+  end
+
+  # Does the cube overlap in any way
+  def overlaps?(cube)
+    x_rng.overlaps?(cube.x_rng) && y_rng.overlaps?(cube.y_rng) && z_rng.overlaps?(cube.z_rng)
+  end
+
+  def volume
+    [x_rng, y_rng, z_rng].map(&:size).inject(:*)
+  end
+
+  def to_a
+    [x_rng, y_rng, z_rng].map(&:to_a).flatten
+  end
+
+  def subtract(cube)
+    # If cube does not overlap then theres nothing to subtract
+    return self unless overlaps?(cube)
+
+    x_ranges, y_ranges, z_ranges = %i[x_rng y_rng z_rng].map { |rng| spans(cube, rng) }
+    remaining_cubes = []
+    x_ranges.each do |x_range, x_cubes|
+      y_ranges.each do |y_range, y_cubes|
+        z_ranges.each do |z_range, z_cubes|
+          # Only create cubes that exclusively belong to the original cube (self) and not the cube being subtracted
+          remaining_cubes << Cube.new(x_range, y_range, z_range) if (x_cubes & y_cubes & z_cubes) == [self]
+        end
+      end
+    end
+    remaining_cubes
+  end
+
+  private
+
+  FIRST = 0
+  LAST = 1
+  def spans(cube, range_sym)
+    points = []
+    points << [send(range_sym).first, FIRST, self]
+    points << [send(range_sym).last,  LAST, self]
+    points << [cube.send(range_sym).first, FIRST, cube]
+    points << [cube.send(range_sym).last, LAST, cube]
+    points.sort! { |p1, p2| p1[0] + p1[1] <=> p2[0] + p2[1] }
+
+    start = 0
+    stack = []
+    span_list = []
+    points.each do |point, edge, c|
+      if edge == FIRST
+        if start.nil? || start == point
+          start = point
+          stack += [c]
+        else
+          span_list << [start..point - 1, stack.dup]
+          start = point
+          stack += [c]
+        end
+      elsif edge == LAST
+        if point >= start
+          span_list << [start..point, stack.dup]
+          stack -= [c]
+          start = point + 1
+        else
+          stack -= [c]
+        end
+      end
+    end
+    # Span list is an array containing [ [range,[cubes in range], ... ] ]
+    span_list
+  end
+end
+
+ONOFF = 0
+CUBE = 1
+instructions = instructions.map do |line|
+  [line[0], Cube.new(line[1][0]..line[1][1], line[1][2]..line[1][3], line[1][4]..line[1][5])]
+end
+
+# Assign first cubic instruction to the reactor (assiming it's an ON which the test set is)
+# Reactor is an array of ON cubes only.
+reactor = [instructions.shift[CUBE]]
+
+instructions.each do |instruction|
+  # Subtract new cube from existing reactor ON cubes
+  reactor = reactor.flat_map { |reactor_cube| reactor_cube.subtract(instruction[CUBE]) }
+  # Add new cube if instruction is set to ON
+  reactor << instruction[CUBE] if instruction[ONOFF] == ON
+end
+
+# Sum volumes of all ON cubes (the only cubes in the reactor array)
+pp reactor.map(&:volume).sum
+
+# 1019187529428416 < TOO LOW
