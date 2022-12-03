2437466238a092a62299c98f5a4112c0ea5234d8
Stuart Ingram <ingrams2@upmc.com>
2022-12-03 07:41:34 -0500

    Day 1 solutions

--- 2022/day-01/day-01-part-1.rb
+++ 2022/day-01/day-01-part-1.rb
@@ -3,4 +3,11 @@
 file_path = File.expand_path("../day-01-input.txt", __FILE__)
 input     = File.read(file_path)
 
+input = input.split("\n").map(&:to_i)
 
+elves = [0]
+input.each do |cal|
+    cal >0 ? elves[0]+=cal : elves.unshift(0)
+end
+
+pp elves.max
--- 2022/day-01/day-01-part-2.rb
+++ 2022/day-01/day-01-part-2.rb
@@ -4,3 +4,16 @@ file_path = File.expand_path("../day-01-input.txt", __FILE__)
 input     = File.read(file_path)
 
 
+#!/usr/bin/env ruby
+
+file_path = File.expand_path("../day-01-input.txt", __FILE__)
+input     = File.read(file_path)
+
+input = input.split("\n").map(&:to_i)
+
+elves = [0]
+input.each do |cal|
+    cal >0 ? elves[0]+=cal : elves.unshift(0)
+end
+
+pp elves.sort.last(3).sum
