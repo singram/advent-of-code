3a61a45e02560cbd79e94fa4222da62008474e65
Stuart Ingram <ingrams2@upmc.com>
2022-12-03 08:31:02 -0500

    Day 3 solutions

--- 2022/day-03/day-03-part-1.rb
+++ 2022/day-03/day-03-part-1.rb
@@ -3,4 +3,17 @@
 file_path = File.expand_path("../day-03-input.txt", __FILE__)
 input     = File.read(file_path)
 
+sacks = input.split("\n")
 
+items_to_rearrange = sacks.map do |sack|
+    items = sack.split('')
+    comp1, comp2 = items.each_slice( (items.size/2.0).round ).to_a
+    (comp1.uniq & comp2.uniq)
+end.flatten
+
+# Lowercase item types a through z have priorities 1 through 26.
+# Uppercase item types A through Z have priorities 27 through 52.
+priorities = items_to_rearrange.map do |i|
+    ("a".."z").include?(i) ? i.ord - 96 :  i.ord - 38
+end
+pp priorities.sum
--- 2022/day-03/day-03-part-2.rb
+++ 2022/day-03/day-03-part-2.rb
@@ -3,4 +3,15 @@
 file_path = File.expand_path("../day-03-input.txt", __FILE__)
 input     = File.read(file_path)
 
+sacks = input.split("\n")
+badges = sacks.each_slice(3).map do |group|
+    group = group.map{ |g| g.split('') }
+    ( group[0].uniq & group[1].uniq & group[2].uniq )
+end.flatten
 
+# Lowercase item types a through z have priorities 1 through 26.
+# Uppercase item types A through Z have priorities 27 through 52.
+priorities = badges.map do |i|
+    ("a".."z").include?(i) ? i.ord - 96 :  i.ord - 38
+end
+pp priorities.sum
