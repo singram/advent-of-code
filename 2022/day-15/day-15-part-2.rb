427f43c8d12aba712e0e83aa1c331570e4edd282
Stuart Ingram <ingrams2@upmc.com>
2022-12-03 07:16:26 -0500

    2022 initialized

---
+++ 2022/day-15/day-15-part-2.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+file_path = File.expand_path("../day-15-input.txt", __FILE__)
+input     = File.read(file_path)
+
+
---
+++ 2022/day-24/day-24-part-1.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+file_path = File.expand_path("../day-24-input.txt", __FILE__)
+input     = File.read(file_path)
+
+
