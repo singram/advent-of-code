2b492671eb2c5caf23f5e194d0f43130c9e7c44d
Stuart Ingram <ingrams2@upmc.com>
2022-12-04 13:34:53 -0500

    Day 4 solutions

--- 2022/day-04/day-04-part-1.rb
+++ 2022/day-04/day-04-part-1.rb
@@ -3,4 +3,12 @@
 file_path = File.expand_path("../day-04-input.txt", __FILE__)
 input     = File.read(file_path)
 
+input = input.split("\n").map{ |line| line.split(/[-,]/).map(&:to_i) }.map{ |a| [(a[0]..a[1]), (a[2]..a[3])] }
 
+class Range
+    def fully_include?(r)
+        (r.first >= self.first && r.last <= self.last)
+    end
+end
+
+pp input.select{|p1,p2| p1.fully_include?(p2) || p2.fully_include?(p1) }.count
--- 2022/day-04/day-04-part-2.rb
+++ 2022/day-04/day-04-part-2.rb
@@ -3,4 +3,10 @@
 file_path = File.expand_path("../day-04-input.txt", __FILE__)
 input     = File.read(file_path)
 
+input = input.split("\n").map{ |line| line.split(/[-,]/).map(&:to_i) }.map{ |a| [(a[0]..a[1]), (a[2]..a[3])] }
 
+pp input.select { |p1,p2| 
+    p1.include?(p2.first) || 
+    p1.include?(p2.last)  || 
+    p2.include?(p1.first) || 
+    p2.include?(p1.last)}.count
