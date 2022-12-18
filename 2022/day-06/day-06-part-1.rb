ae00b4cfe28f02a04ea6c9111dd56e18f3d73998
Stuart Ingram <ingrams2@upmc.com>
2022-12-18 10:47:41 -0500

    Day 6 solutions

--- 2022/day-06/day-06-part-1.rb
+++ 2022/day-06/day-06-part-1.rb
@@ -3,4 +3,12 @@
 file_path = File.expand_path("../day-06-input.txt", __FILE__)
 input     = File.read(file_path)
 
+MKR_SIZE = 4
 
+input = input.split('')
+
+idx = (0..input.size-1).find do |idx|
+    input.slice(idx, MKR_SIZE).uniq.size == MKR_SIZE
+end
+
+pp idx + MKR_SIZE
