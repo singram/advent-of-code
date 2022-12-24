ea6282e0dd7c376b64eabd59853b4a11f0d806e2
Stuart Ingram <ingrams2@upmc.com>
2022-12-24 14:34:05 -0500

    Day 8 solutions

--- 2022/day-08/day-08-part-1.rb
+++ 2022/day-08/day-08-part-1.rb
@@ -3,4 +3,35 @@
 file_path = File.expand_path("../day-08-input.txt", __FILE__)
 input     = File.read(file_path)
 
+tree_height_map = input.split("\n").map{|line| line.split('').map(&:to_i)}
+WIDTH = tree_height_map[0].size
+HEIGHT = tree_height_map.size
+tree_visible_map = Array.new(HEIGHT) { Array.new(WIDTH, false) }
 
+# Rotates a nested arrary clockwise
+def rotate_clockwise(arr)
+    arr.transpose.map(&:reverse)
+end
+
+# Test each row from left to right until the tallest tree is hit.
+# Rotate the map 4 times to test all sides.
+4.times do
+    tree_height_map.each_with_index do |line, j|
+        max_height = line.max
+        current_height = -1
+        line.each_with_index do |height, i|
+            if height > current_height
+                current_height = height
+                tree_visible_map[j][i] = true
+            end
+            break if current_height == max_height
+        end
+    end
+
+    tree_height_map = rotate_clockwise(tree_height_map)
+    tree_visible_map = rotate_clockwise(tree_visible_map)
+end
+
+# pp tree_height_map
+# pp tree_visible_map
+pp tree_visible_map.flatten.count(true)
--- 2022/day-08/day-08-part-2.rb
+++ 2022/day-08/day-08-part-2.rb
@@ -3,4 +3,42 @@
 file_path = File.expand_path("../day-08-input.txt", __FILE__)
 input     = File.read(file_path)
 
+tree_height_map = input.split("\n").map{|line| line.split('').map(&:to_i)}
+WIDTH = tree_height_map[0].size
+HEIGHT = tree_height_map.size
+tree_value_map = Array.new(HEIGHT) { Array.new(WIDTH, nil) }
 
+Y = 0
+X = 1
+
+VEC = {
+    UP:    [-1,  0],
+    DOWN:  [ 1,  0],
+    LEFT:  [ 0, -1],
+    RIGHT: [ 0,  1]
+}
+
+# Iterate over every position recording score.
+# Note.  There are more efficient ways to do this with simply scanning a
+# row in one direction and recording all values on a line for a given vector then multiplying 
+# at the end.
+tree_height_map.each_with_index do |line, j|
+    line.each_with_index do |height, i|
+        start_height = tree_height_map[j][i]
+        tree_value_map[j][i] = VEC.keys.map do |dir|
+            position = [j,i]
+            count = 0
+            while true
+                position = [position[Y] + VEC[dir][Y], position[X] + VEC[dir][X]]
+                break unless position[Y].between?(0, HEIGHT-1) && position[X].between?(0, WIDTH-1)
+                count += 1
+                break if tree_height_map[position[Y]][position[X]] >= start_height           
+            end
+            count
+        end.inject(:*)
+    end
+end
+
+#  pp tree_height_map
+#  pp tree_value_map
+ pp tree_value_map.map(&:max).max
