273fcdc1de68dd5684fc6dab560763c6c275c7d9
Stuart Ingram <ingrams2@upmc.com>
2022-12-28 08:07:34 -0500

    Day 10 solutions

--- 2022/day-10/day-10-part-1.rb
+++ 2022/day-10/day-10-part-1.rb
@@ -3,4 +3,29 @@
 file_path = File.expand_path("../day-10-input.txt", __FILE__)
 input     = File.read(file_path)
 
+CLOCK_COST = {
+    'noop' => 1,
+    'addx' => 2
+}
+
+costs = {}
+
+x = 1
+clock = 0
+next_trigger = 20
+TRIGGER_FREQ = 40
+TRIGGER_MAX = 221
+
+input.split("\n").map{ |l| l.split(' ') }.each do |inst, val|
+    if clock+CLOCK_COST[inst] >= next_trigger
+        costs[next_trigger] = x
+        next_trigger += TRIGGER_FREQ
+    end
+    clock += CLOCK_COST[inst]
+    x += val.to_i if inst == 'addx'
+    break if TRIGGER_MAX < clock
+end
+
+# pp costs
+pp costs.keys.map{|k| k * costs[k]}.sum
 
--- 2022/day-10/day-10-part-2.rb
+++ 2022/day-10/day-10-part-2.rb
@@ -3,4 +3,32 @@
 file_path = File.expand_path("../day-10-input.txt", __FILE__)
 input     = File.read(file_path)
 
+CLOCK_COST = {
+    'noop' => 1,
+    'addx' => 2
+}
+
+SCREEN_WIDTH = 40
+
+def sprite_rng(x)
+    (x-1..x+1)
+end
+
+def print_pixel(clock, x)
+    print sprite_rng(x).include?((clock-1) % SCREEN_WIDTH) ? '#' : '.'
+    print "\n" if clock % SCREEN_WIDTH == 0
+end
+
+clock = 0
+x = 1
+
+input.split("\n").map{ |l| l.split(' ') }.each do |inst, val|
+    if CLOCK_COST[inst] > 1
+        clock +=1
+        print_pixel(clock, x)
+    end
+    clock += 1
+    print_pixel(clock, x)
+    x += val.to_i if inst == 'addx'
+end
 
