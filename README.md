785cead2c25d895d62a933de1f222f50d8d2b71f
Stuart Ingram <ingrams2@upmc.com>
2023-03-13 16:17:26 -0400

    Github action to update AoC readme stats

--- README.md
+++ README.md
@@ -4,6 +4,16 @@
 
 Solutions are written in [Ruby](https://www.ruby-lang.org/en/).
 
+<!--START_SECTION:stats-->
+| Year | Attempt between 📅| Stats |
+| ---- | ---- | ---- |
+| 2022 | December  3, 2022 -> December 28, 2022 |  ![](https://img.shields.io/badge/Stars%20⭐-20-yellow)  ![](https://img.shields.io/badge/Days%20completed-10-red) | 
+| 2021 | December  1, 2021 -> September  4, 2022 |  ![](https://img.shields.io/badge/Stars%20⭐-40-yellow)  ![](https://img.shields.io/badge/Days%20completed-19-red) | 
+| 2020 | December  4, 2020 -> April 29, 2021 |  ![](https://img.shields.io/badge/Stars%20⭐-25-yellow)  ![](https://img.shields.io/badge/Days%20completed-12-red) | 
+
+<sub>*(updated @ 2023-03-13 16:36:03 -0400). Inspired by [AoC-badges](https://github.com/J0B10/aoc-badges-action)*</sub>
+<!--END_SECTION:stats-->
+
 ### What is Advent of Code?
 
 _"Advent of Code is a series of small programming puzzles for a variety of skill levels. They are self-contained and are just as appropriate for an expert who wants to stay sharp as they are for a beginner who is just learning to code. Each puzzle calls upon different skills and has two parts that build on a theme."_
---
+++ update_readme_badges.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+require 'net/http'
+require 'cgi'
+require 'json'
+require 'date'
+
+USER_ID     = ENV['USER_ID']
+SESSION_ID  = ENV['SESSION_ID']
+START_TOKEN = ENV['START_TOKEN'] || "<!--START_SECTION:stats-->"
+END_TOKEN   = ENV['END_TOKEN'] || "<!--END_SECTION:stats-->"
+README      = ENV['README'] || 'README.md'
+
+data = {}
+cookies = {'session': SESSION_ID }
+years =  Dir.glob('*').select {|f| File.directory?(f) && f =~ /\d{4}/}
+
+begin
+    years.each do |year|
+        uri = URI("https://adventofcode.com/#{year}/leaderboard/private/view/#{USER_ID}.json")
+        req = Net::HTTP::Get.new(uri)
+        req[:Cookie] = CGI::Cookie.new('session', SESSION_ID).to_s
+        res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) {|http| http.request(req) }
+        body = JSON.parse(res.body)
+        times = body['members'][USER_ID]['completion_day_level'].flat_map{|day, info| info.map{|day, day_data| day_data['get_star_ts']} }
+        data[year.to_i] = { 
+            stars: body['members'][USER_ID]['stars'],
+            days: body['members'][USER_ID]['completion_day_level'].count{|day, info| info.size == 2},
+            first: times.min,
+            last: times.max
+        }
+    end
+rescue => e
+    data = nil
+    pp e
+end
+
+if data
+    stats_insert  = "\n| Year | Attempt between 📅| Stats |\n"
+    stats_insert += "| ---- | ---- | ---- |\n"
+    data.each do |year, stats|
+        stats_insert += "| #{year} "
+        stats_insert += "| #{Time.at(stats[:first]).strftime("%B %e, %Y")} "
+        stats_insert += "-> #{Time.at(stats[:last]).strftime("%B %e, %Y")} | "
+        stats_insert += " ![](https://img.shields.io/badge/Stars%20⭐-#{stats[:stars]}-yellow) "
+        stats_insert += " ![](https://img.shields.io/badge/Days%20completed-#{stats[:days]}-red) | \n"
+    end
+    stats_insert += "\n<sub>*(updated @ #{Time.now}). Inspired by [AoC-badges](https://github.com/J0B10/aoc-badges-action)*</sub>\n"
+    
+    readme = File.open(README).read
+    readme.sub!(/#{START_TOKEN}.*#{END_TOKEN}/mi, "#{START_TOKEN}#{stats_insert}#{END_TOKEN}")
+    File.write(README, readme)
+end
+
