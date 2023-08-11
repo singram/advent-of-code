71336796523067cd748c519391c43ac55194d230
Stuart Ingram <ingrams2@upmc.com>
2023-08-11 13:57:01 -0400

    Day 7 solutions

--- 2022/day-07/day-07-part-1.rb
+++ 2022/day-07/day-07-part-1.rb
@@ -2,76 +2,31 @@
 
 file_path = File.expand_path("../day-07-input.txt", __FILE__)
 input     = File.read(file_path)
-input = "$ cd /
-$ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d
-$ cd a
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k"
 
 input     = input.split("\n")
 
-SEPARATER = '/'
-ROOT_DIR = '/'
-current_dir  = ROOT_DIR
+require 'pathname'
+
+current_dir  = Pathname.new('/')
 file_sys = {}
+dirs = { current_dir => true}
 
 # Build out filesystem map 'dir+filename' -> size
 input.each do |cmd|
-    # pp '----------------'
-    # pp cmd
     if cmd =~ /\$ cd (.*)$/i 
-        dir = $1
-        if dir == '..'
-            # current_dir = current_dir.gsub(/\/(\w+)$/, "")
-            current_dir = File.dirname(current_dir)
-        elsif dir == ROOT_DIR
-            current_dir = ROOT_DIR
-        else
-            current_dir += $1 + SEPARATER  
-        end
-    #   puts "CD -> #{$1}"
-      
-    #   current_dir = $1 == ROOT_DIR ? ROOT_DIR : current_dir += SEPARATER + $1
-    elsif cmd =~ /\$ ls$/i
-        next
-    elsif cmd =~ /(.*) (.*)/ 
-        file_sys[current_dir+$2] = $1.to_i unless $1 == 'dir'
+        current_dir = $1 == '/' ? Pathname.new('/') : current_dir + $1
+    elsif cmd =~ /dir (.*)/ 
+        # Assumption: All dirs are explicitly mentioned in input file.
+        dirs[current_dir+$1] = true
+    elsif cmd =~ /(\d+) (.*)/ 
+        file_sys[current_dir+$2] = $1.to_i 
     end
-    # pp current_dir
-    # pp file_sys
 end
 
-# Extract all unique paths including root
-paths = file_sys.map{ |file_path,size| File.dirname(file_path) }.uniq
-
-# Calculate total size of paths including sub directories
-path_totals = {}
-paths.each do |path| 
-    path_totals[path] = file_sys.select{|fspath, fssize| fspath.start_with?(path)}.values.inject(&:+)
+dir_totals = {}
+dirs.each do |dir, x|
+    dir_totals[dir] = file_sys.select{|fspath, fssize| fspath.to_s.start_with?(dir.to_s)}.values.sum
 end
 
-# Output the sum of all paths <= 100000 in size
-pp path_totals.select{ |fspath, fssize| fssize <= 100000 }.values.inject(&:+)
-
-# pp file_sys
-# pp paths
- pp path_totals
+pp dir_totals.select{ |fspath, fssize| fssize <= 100000 }.values.sum
 
--- 2022/day-07/day-07-part-2.rb
+++ 2022/day-07/day-07-part-2.rb
@@ -3,4 +3,31 @@
 file_path = File.expand_path("../day-07-input.txt", __FILE__)
 input     = File.read(file_path)
 
+input     = input.split("\n")
+
+require 'pathname'
+
+current_dir  = Pathname.new('/')
+file_sys = {}
+dirs = { current_dir => true}
+
+# Build out filesystem map 'dir+filename' -> size
+input.each do |cmd|
+    if cmd =~ /\$ cd (.*)$/i 
+        current_dir = $1 == '/' ? Pathname.new('/') : current_dir + $1
+    elsif cmd =~ /dir (.*)/ 
+        # Assumption: All dirs are explicitly mentioned in input file.
+        dirs[current_dir+$1] = true
+    elsif cmd =~ /(\d+) (.*)/ 
+        file_sys[current_dir+$2] = $1.to_i 
+    end
+end
+
+dir_totals = {}
+dirs.each do |dir, x|
+    dir_totals[dir] = file_sys.select{|fspath, fssize| fspath.to_s.start_with?(dir.to_s)}.values.sum
+end
+
+space_needed = 30000000 - (70000000 - dir_totals[Pathname.new('/')])
+pp dir_totals.values.sort.find{|a| a >= space_needed}
 
