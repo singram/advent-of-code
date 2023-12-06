6840657f6216df32cbfbd30cae23178bc5c0e72c
Stuart Ingram <ingrams2@upmc.com>
2023-12-06 12:33:06 -0500

    Day 4 solutions

--- 2023/day-04/day-04-part-1.rb
+++ 2023/day-04/day-04-part-1.rb
@@ -3,4 +3,16 @@
 file_path = File.expand_path("../day-04-input.txt", __FILE__)
 input     = File.read(file_path)
 
+results = input.split("\n").map do |line|
+    (game, number_line) = line.split(':')
+    game = game.scan(/\d+/)[0].to_i
+    (card_str, winners_str) = number_line.split('|')
+    card    = card_str.split(' ').map(&:to_i)
+    winners = winners_str.split(' ').map(&:to_i)
+    matches = card & winners
+    matches.size == 0 ? 0 : 2**(matches.size-1)
+end
+
+pp results.sum
+
 
--- 2023/day-04/day-04-part-2.rb
+++ 2023/day-04/day-04-part-2.rb
@@ -3,4 +3,20 @@
 file_path = File.expand_path("../day-04-input.txt", __FILE__)
 input     = File.read(file_path)
 
+card_count = 0
+copy_stack = []
+results = input.split("\n").each do |line|
+    (game, number_line) = line.split(':')
+    game = game.scan(/\d+/)[0].to_i
+    (card_str, winners_str) = number_line.split('|')
+    card    = card_str.split(' ').map(&:to_i)
+    winners = winners_str.split(' ').map(&:to_i)
+    matches = (card & winners).size
+    total_wins = 1 + (copy_stack.shift || 0)
+    card_count += total_wins
+    (0...matches).to_a.each{|i| copy_stack[i] = (copy_stack[i] || 0) + total_wins }
+end
+
+pp card_count
+
 
