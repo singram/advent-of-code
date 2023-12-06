1f125915c74962d078d29ce323da5522620daf3c
Stuart Ingram <ingrams2@upmc.com>
2023-12-06 08:43:14 -0500

    Day 2 solutions

--- 2023/day-02/day-02-part-1.rb
+++ 2023/day-02/day-02-part-1.rb
@@ -3,4 +3,21 @@
 file_path = File.expand_path("../day-02-input.txt", __FILE__)
 input     = File.read(file_path)
 
+constraints = { red: 12, green: 13, blue: 14}
 
+games = {}
+input.split("\n") do |line|
+    (game, sets) = line.split(':')
+    game = game.scan(/\d+/)[0].to_i
+    games[game] = []
+    sets.split(";").each do |set|
+        h = {}
+        set.split(',').each do |combo|
+            (num, col) = combo.strip.split(' ')
+            h[col.to_sym] = num.to_i
+        end
+        games[game] << h
+    end
+end
+
+pp games.select{|games, sets| sets.all?{|set| set.all?{|col,num| constraints[col] >= num } } }.keys.sum
--- 2023/day-02/day-02-part-2.rb
+++ 2023/day-02/day-02-part-2.rb
@@ -3,4 +3,21 @@
 file_path = File.expand_path("../day-02-input.txt", __FILE__)
 input     = File.read(file_path)
 
+games = {}
+input.split("\n") do |line|
+    (game, sets) = line.split(':')
+    game = game.scan(/\d+/)[0].to_i
+    games[game] = {}
+    sets.split(";").each do |set|
+        set.split(',').each do |combo|
+            (num, color) = combo.strip.split(' ')
+            color = color.to_sym
+            num = num.to_i
+            games[game][color] ||= num
+            games[game][color] = num if num > games[game][color]
+        end
+    end
+    games[game] = games[game].values.inject(:*)
+end
 
+pp games.values.sum
