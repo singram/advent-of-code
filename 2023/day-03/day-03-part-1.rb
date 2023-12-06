4bbf2db3a7fc3a45820b341dd5a942bbf505a477
Stuart Ingram <ingrams2@upmc.com>
2023-12-06 15:57:19 -0500

    Day 3 solutions

--- 2023/day-03/day-03-part-1.rb
+++ 2023/day-03/day-03-part-1.rb
@@ -3,4 +3,41 @@
 file_path = File.expand_path("../day-03-input.txt", __FILE__)
 input     = File.read(file_path)
 
+SYMBOL_PROXIMITY = '*'
+EMPTY = '.'
+map        = input.split("\n").map{|l| l.split('')}
 
+# Create proximity map for symbols in map
+ROWS = map.size
+COLS = map[0].size
+symbol_map = Array.new(ROWS) { Array.new(COLS, EMPTY) }
+map.each_with_index do | row, row_index |
+    row.each_with_index do | element, col_index|
+        next if element.match?(/[0-9\.]/) # Ignore if the element is a number or empty position.
+        [-1,0,1].each do |row_offset|
+            [-1,0,1].each do |col_offset|
+                tgt_row = row_index + row_offset
+                tgt_col = col_index + col_offset
+                if tgt_col.between?(0,COLS-1) && tgt_row.between?(0,ROWS-1)
+                    symbol_map[tgt_row][tgt_col] = SYMBOL_PROXIMITY
+                end
+            end
+        end
+    end
+end
+
+map        = map.map(&:join)
+symbol_map = symbol_map.map(&:join)
+
+num_sum = 0
+map.each do |row|
+    mask = symbol_map.shift
+    row.enum_for(:scan, /\d+/).map { Regexp.last_match }.each do |match|
+        number =  match[0].to_i
+        position = match.begin(0) 
+        mask_match = mask[position, match[0].size]
+        num_sum += number if mask_match.include?('*')
+    end
+end
+
+pp num_sum
--- 2023/day-03/day-03-part-2.rb
+++ 2023/day-03/day-03-part-2.rb
@@ -3,4 +3,34 @@
 file_path = File.expand_path("../day-03-input.txt", __FILE__)
 input     = File.read(file_path)
 
+map        = input.split("\n") 
+
+# Assumption: Numbers are distributed meaning there are no duplicates in close proximity
+num_map = {}
+map.each_with_index do | row, row_index |
+    row.enum_for(:scan, /\d+/).map { Regexp.last_match }.each do |match|
+        number =  match[0].to_i
+        position = match.begin(0) 
+        (0...match[0].size).each {|i| num_map[[row_index, position+i]] = number}
+    end
+end
+
+# Iterate over maps looking for '*' instances and check number map for adjacent parts
+sum_gear_ratio = 0
+map.each_with_index do | row, row_index |
+    row.enum_for(:scan, '*').map { Regexp.last_match }.each do |match|
+        part_numbers = []
+        ref_col = match.begin(0) 
+        [-1,0,1].each do |row_offset|
+            [-1,0,1].each do |col_offset|
+                part_numbers << num_map[[row_index+row_offset, ref_col+col_offset]]
+            end
+        end
+        part_numbers = part_numbers.compact.uniq
+        
+        sum_gear_ratio += part_numbers.inject(&:*) if part_numbers.size == 2
+    end
+end
+
+pp sum_gear_ratio
 
